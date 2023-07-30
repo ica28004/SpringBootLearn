bde548da4ff2efdd21c2345dcd25ac58437f0326
ica28 <ica28@COOLERMASTER>
2023-07-30 11:00:27 +0900

    リクエストマッピング変更

--- spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot1/src/main/java/com/example/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	public String index() {
 		// hello.htmlを呼び出す
 		return "hello";

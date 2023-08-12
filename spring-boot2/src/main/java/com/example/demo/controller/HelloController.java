70ba594258b6b39f1a121a43412c9e1889c78d30
ica28 <ica28@COOLERMASTER>
2023-08-12 16:25:47 +0900

    修正

--- spring-boot2/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot2/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
 
-import org. springframework. stereotype. Controller;
-import org. springframework. ui. Model;
-import org. springframework. web. bind. annotation. GetMapping;
-import org. springframework. web. bind. annotation. RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
